489ed2a9b4de27ec8ba12fd62d04118ee0956d9c
ksowah <sowahkelvin640@gmail.com>
2022-06-20 06:27:38 +0700

    add category screen

---
+++ screens/Category.js
@@ -0,0 +1,35 @@
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import React from "react";
+import tw from "twrnc";
+import Nav from "../components/Nav";
+import { foodItems } from "../foodItems";
+import Products from "../components/Products";
+import Tabs from "../components/Tabs";
+
+const Category = ({ navigation, route }) => {
+	return (
+		<View style={tw`flex-1 bg-[#EBEBEB]`}>
+			<SafeAreaView style={tw`flex-1`}>
+				<Nav method={() => navigation.goBack()} title={route.params.category}/>
+				<ScrollView>
+					<View style={tw`mt-4 items-center justify-center flex-row flex-wrap`}>
+						{foodItems.map(
+							(foodItem, idx) =>
+								foodItem.category === route.params.category && (
+									<Products
+										key={idx}
+										image={foodItem.image}
+										price={foodItem.price}
+										name={foodItem.name}
+									/>
+								)
+						)}
+					</View>
+				</ScrollView>
+				<Tabs/>
+			</SafeAreaView>
+		</View>
+	);
+};
+
+export default Category;
