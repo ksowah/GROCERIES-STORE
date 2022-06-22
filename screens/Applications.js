572bbe5cc9a73c89b968c88aa4a1cb087e5279ed
ksowah <sowahkelvin640@gmail.com>
2022-06-22 22:43:59 +0000

    create app screen

---
+++ screens/Applications.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Applications = () => {
+  return (
+    <View>
+      <Text>Applications</Text>
+    </View>
+  )
+}
+
+export default Applications
